8a7a6e06c3782019320ae3a53ba31c57b8a48625
Nedjma <bouamaranedjma@gmail.com>
2023-05-09 00:40:51 +0200

    Added comparison plot

--- Results/script_plot.sh
+++ Results/script_plot.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set title "{/:Bold Comparaison avec le flag -O3 }"
+set title "{/:Bold Comparaison avec les flags d'optimisation }"
 set title font "arial,14"
-set border 3 front lt black linewidth 1.000 dashtype solid
-set style data histogram 
+set border 3 front lt black linewidth 1.000  solid
+set style data histograms
 set style histogram clustered gap 1
-set style fill solid noborder
+set style fill solid border -1
 set datafile separator " "
-plot newhistogram "o2", "result1.txt" u 2:xtic(" ") t "Normal" lc rgbcolor "blue" lt 1, "result2.txt" u 2:xtic(" ") t "avec parallélisme" lc rgbcolor "green" lt 1
-
+set boxwidth 0.9 relative
+plot  "result.txt" using 2:xtic(1) title "No Flags",\
+"result-O2.txt" using 2:xtic(1) title "O2",\
+"result-O3.txt" using 2:xtic(1) title "O3",\
+"result-Ofast.txt" using 2:xtic(1) title "Ofast"
